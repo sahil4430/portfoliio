5c57984956fac109c289af4b8d4901e11a51680b
sahil4430 <panwarsahil204@gmail.com>
2024-06-07 21:58:05 +0530

    home page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,76 @@
 import React from 'react'
 import './home.css';
 import NavBar from './NavBar';
+import { experimentalStyled as styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 export default function Home() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
   return (
     <>
     <div className='body'>
     <NavBar></NavBar>
-    <h1>
+    <br/>
+    
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={5}>
+        
+        </Grid>
+        <Grid item xs={2}>
+        <h1>
       Sahil <br/>
       Panwar
     </h1>
+        </Grid>
+        <Grid item xs={5}>
+        </Grid>
+      </Grid>
+    </Box>
+    <Box sx={{ flexGrow: 1 } } className =" con">
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+      <h2>Services</h2>
+      <br></br>
+           <div className='app'>
+            <img src='.\1one.png'className='ic1'></img>
+            <h5 className='p1'>App development</h5>
+            
+           </div>
+           <p className='p2'>I am a App frontend <br/>developer (using flutter).
+            </p>
+            <br></br>
+            <div className='app'>
+
+            <img src='.\2one.png'className='ic1'></img>
+            <h5 className='p1'>UI/UX Design</h5>
+            
+           </div>
+           <p className='p2'>UI/Ux Designer in<br/> GDSC MM(DU).
+            </p>
+            <br></br>
+            <div className='app'>
+
+            <img src='.\3rd.png'className='ic1'></img>
+            <h5 className='p1'>  Mern Stack</h5>
+          </div>
+          <h6 className='p2'>I am a App frontend developer (using flutter).
+            </h6>
+        </Grid>
+        <Grid item xs={4}>
+       44
+        </Grid>
+        <Grid item xs={4}>44
+        </Grid>
+      </Grid>
+    </Box>
     </div>
     </>
     
